d6d77d846dc0177920f2402886670932a152d317
thecheesemister <146137818+thecheesemister@users.noreply.github.com>
2025-04-08 09:00:02 -0700

    Update and rename script.js to main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#333',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+let car;
+let cursors;
+
+function preload() {
+  this.load.image('car', 'https://i.imgur.com/5LQAvOZ.png'); // top-down car sprite
+}
+
+function create() {
+  car = this.physics.add.sprite(400, 300, 'car');
+  car.setDamping(true);
+  car.setDrag(0.98);
+  car.setMaxVelocity(200);
+
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  if (cursors.up.isDown) {
+    this.physics.velocityFromRotation(car.rotation, 200, car.body.acceleration);
+  } else {
+    car.setAcceleration(0);
+  }
+
+  if (cursors.left.isDown) {
+    car.setAngularVelocity(-150);
+  } else if (cursors.right.isDown) {
+    car.setAngularVelocity(150);
+  } else {
+    car.setAngularVelocity(0);
+  }
+}
